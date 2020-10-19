d29480ef17dc0fb9bd32138eff4e8a2605d18ca7
Peter Mukuku <mukukup@gmail.com>
2020-10-19 23:36:23 +0200

    Add files via upload

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,35 +1,107 @@
-import { NgModule, ErrorHandler } from '@angular/core';
+//native modules
 import { BrowserModule } from '@angular/platform-browser';
-import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
+import { ErrorHandler, NgModule } from '@angular/core';
+import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
+import { SplashScreen } from '@ionic-native/splash-screen';
+import { IonicStorageModule } from '@ionic/storage';
+import { StatusBar } from '@ionic-native/status-bar';
+import { Camera } from '@ionic-native/camera';
+// import { BackgroundGeolocation } from '@ionic-native/background-geolocation';
+import { Geolocation } from '@ionic-native/geolocation';
+import { Facebook } from '@ionic-native/facebook';
+import { Vibration } from '@ionic-native/vibration';
+import { BrowserTab} from '@ionic-native/browser-tab';
+import {
+  GoogleMaps,
+  GoogleMap,
+  GoogleMapsEvent,
+  GoogleMapOptions,
+  CameraPosition,
+  MarkerOptions,
+  Marker
+ } from '@ionic-native/google-maps';
+// import { AngularFireModule } from 'angularfire2';
+// import { AngularFireDatabaseModule } from 'angularfire2/database';
+// import { AngularFireAuthModule } from 'angularfire2/auth';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import * as firebase from 'firebase/app'
+import { OneSignal} from '@ionic-native/onesignal';
+import { Diagnostic } from '@ionic-native/diagnostic';
+import { InAppBrowser } from '@ionic-native/in-app-browser';
+import { CallNumber } from '@ionic-native/call-number';
+import { Ionic2RatingModule } from 'ionic2-rating';
+//other
 import { MyApp } from './app.component';
 
-import { AboutPage } from '../pages/about/about';
-import { ContactPage } from '../pages/contact/contact';
-import { HomePage } from '../pages/home/home';
-import { TabsPage } from '../pages/tabs/tabs';
+//providers
+import { AuthProvider } from '../providers/auth/auth';
+import { EventProvider } from '../providers/event/event';
+import { ProfileProvider } from '../providers/profile/profile';
+import { RatePage } from '../pages/rate/rate';
+import { MapContainerProvider } from '../providers/map-container/map-container';
+import { GeocoderProvider } from '../providers/geocoder/geocoder';
+import { DirectionserviceProvider } from '../providers/directionservice/directionservice';
+import { AnimControlProvider } from '../providers/anim-control/anim-control';
+import { PopUpProvider } from '../providers/pop-up/pop-up';
+import { OnesignalProvider } from '../providers/onesignal/onesignal';
+import { NativeMapContainerProvider } from '../providers/native-map-container/native-map-container';
+
+export const firebaseConfig = {
+  apiKey: "XXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
+  authDomain: "XXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
+  databaseURL: "XXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
+  storageBucket: "XXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
+  messagingSenderId: "XXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
+  
+};
+
+firebase.initializeApp(firebaseConfig);
 
 @NgModule({
   declarations: [
     MyApp,
-    AboutPage,
-    ContactPage,
-    HomePage,
-    TabsPage
+    RatePage,
+   
+
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    Ionic2RatingModule,
+    IonicStorageModule.forRoot(),
+    BrowserModule,
+    BrowserAnimationsModule,
   ],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    AboutPage,
-    ContactPage,
-    HomePage,
-    TabsPage
+    RatePage,
   ],
   providers: [
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    StatusBar,
+    SplashScreen,
+    {provide: ErrorHandler, useClass: IonicErrorHandler}, 
+    AuthProvider, 
+    EventProvider, 
+    ProfileProvider,
+    // BackgroundGeolocation,
+    BrowserTab,
+    Geolocation,
+    MapContainerProvider,
+    Facebook,
+    GeocoderProvider,
+    DirectionserviceProvider,
+    AnimControlProvider,
+    PopUpProvider,
+    OnesignalProvider,
+    OneSignal,
+    Diagnostic,
+    InAppBrowser,
+    CallNumber,
+    RatePage,
+    NativeMapContainerProvider,
+    GoogleMaps,
+    Vibration
   ]
 })
 export class AppModule {}
--- app/main.ts
+++ app/main.ts
@@ -1,15 +1,7 @@
-import './polyfills';
-
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
+import { enableProdMode} from '@angular/core';
 import { AppModule } from './app.module';
-
-platformBrowserDynamic().bootstrapModule(AppModule).then(ref => {
-  // Ensure Angular destroys itself on hot reloads.
-  if (window['ngRef']) {
-    window['ngRef'].destroy();
-  }
-  window['ngRef'] = ref;
-
-  // Otherise, log the boot error
-}).catch(err => console.error(err));
+ 
+// this is the magic wand
+enableProdMode();
+platformBrowserDynamic().bootstrapModule(AppModule);
